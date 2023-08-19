daeb7dd8823e694f36a9cfece4efea89b46c9b89
mwoo-git <hanulbom@gmail.com>
2023-08-20 02:34:37 +0900

    add: listHeader 추가

--- lib/screens/trade/w_trade_list.dart
+++ lib/screens/trade/w_trade_list.dart
@@ -18,13 +18,34 @@ class TradeListView extends StatelessWidget {
           reverse: false,
           itemCount: vm.tradelist.length,
           itemBuilder: (context, index) {
-            final ticker = vm.tradelist[index];
-            return listTileView(ticker);
+            if (index == 0) {
+              return listHeader;
+            } else {
+              // 나머지 경우 기존의 listTileView 위젯 반환
+              final ticker = vm.tradelist[index - 1]; // 추가한 Row 위젯 때문에 인덱스를 -1
+              return listTileView(ticker);
+            }
           },
         );
       }
     });
   }
+
+  Widget get listHeader => const Padding(
+        padding: EdgeInsets.only(left: 18, right: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                SizedBox(width: 100, child: Text('시간')),
+                Text('가격'),
+              ],
+            ),
+            Text('거래대금'),
+          ],
+        ),
+      );
 
   ListTile listTileView(TradeTileViewModel ticker) {
     return ListTile(
