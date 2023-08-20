29844900bbdc902dd53b1f32389c9e99b91e9a88
mwoo-git <hanulbom@gmail.com>
2023-08-20 21:09:22 +0900

    add: 탭하면 TradeView의 타이틀 전환되도록 구현

--- lib/screens/trade/v_trade.dart
+++ lib/screens/trade/v_trade.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:mini_trade_flutter/global/api/binance_socket.dart';
 import 'package:mini_trade_flutter/screens/trade/w_trade_list.dart';
 
-class TradeView extends StatelessWidget {
+class TradeView extends StatefulWidget {
   const TradeView({super.key});
+
+  @override
+  State<TradeView> createState() => _TradeViewState();
+}
+
+class _TradeViewState extends State<TradeView> {
+  String curruntCoin = 'BTCUSDT';
+
+  @override
+  void initState() {
+    super.initState();
+    obserber();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,12 +27,20 @@ class TradeView extends StatelessWidget {
     );
   }
 
+  void obserber() {
+    ever(BinanceWebSocketService.currentCoin, (coin) {
+      setState(() {
+        curruntCoin = coin ?? 'BTCUSDT';
+      });
+    });
+  }
+
   AppBar appBar() => AppBar(
         leading: IconButton(
           onPressed: () {},
           icon: const Icon(Icons.refresh),
         ),
-        title: const Text('BTCUSDT 미니체결'),
+        title: Text('$curruntCoin 미니체결'),
         actions: [
           IconButton(
             onPressed: () {},
