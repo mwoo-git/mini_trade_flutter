0815aeab30206685ce47a005824e54c4a5a5e66f
mwoo-git <hanulbom@gmail.com>
2023-09-11 03:20:41 +0900

    feat: 소리 및 진동 발생 기능 추가

--- lib/screens/trade/vm_trade_list.dart
+++ lib/screens/trade/vm_trade_list.dart
@@ -4,6 +4,9 @@ import 'package:mini_trade_flutter/global/data/prefs.dart';
 import 'package:mini_trade_flutter/screens/trade/vm_trade_tile.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:mini_trade_flutter/screens/v_main_tab.dart';
+import 'package:sound_mode/sound_mode.dart';
+import 'package:sound_mode/utils/ringer_mode_statuses.dart';
+import 'package:vibration/vibration.dart';
 
 class TradeListViewModel extends GetxController {
   final BinanceWebSocketService socket = Get.find<BinanceWebSocketService>();
@@ -11,24 +14,27 @@ class TradeListViewModel extends GetxController {
 
   final player = AudioPlayer();
 
+  bool? hasVibrator;
+
   @override
   void onInit() {
     super.onInit();
 
     observer();
+    configure();
   }
 
-  void observer() async {
+  configure() async {
+    hasVibrator = await Vibration.hasVibrator();
+  }
+
+  observer() async {
     ever(
       BinanceWebSocketService.vm,
       (vm) async {
         if (vm != null) {
           tradelist.insert(0, vm);
-
-          if (vm.amounInt > 50000 && MainTabView.currentIndex.value == 1) {
-            await player.stop();
-            await player.play(AssetSource('sounds/01.wav'));
-          }
+          playSoundOrVibrateIfPossible(vm.amounInt);
         }
         if (tradelist.length > 15) {
           tradelist.removeLast();
@@ -51,7 +57,25 @@ class TradeListViewModel extends GetxController {
     );
   }
 
-  void clearList() {
+  playSoundOrVibrateIfPossible(int amount) async {
+    if (amount > 100000 && MainTabView.currentIndex.value == 1) {
+      switch (await SoundMode.ringerModeStatus) {
+        case RingerModeStatus.silent:
+          break;
+        case RingerModeStatus.vibrate:
+          if (hasVibrator == true) {
+            Vibration.vibrate();
+          }
+        case RingerModeStatus.normal:
+          await player.stop();
+          await player.play(AssetSource('sounds/01.wav'));
+        default:
+          break;
+      }
+    }
+  }
+
+  clearList() {
     tradelist.value = [];
   }
 }
